c648041cc3692a6864ee77884124cf07d653d049
Brian Song <brian.cs.song@gmail.com>
2024-10-25 17:36:48 +0000

    Until square root

--- app.rb
+++ app.rb
@@ -13,8 +13,24 @@ get("/square/new") do
   erb(:new_square_calc)
 end
 
-get '/square/results' do
-  "Hello World"
+get ("/square/results") do
+  @the_num = params.fetch("number").to_f
+
+  @the_result = @the_num ** 2
+  
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get ("/square_root/results") do
+  @the_num = params.fetch("user_number").to_f
+
+  @the_result = Math.sqrt(@the_num)
+  
+  erb(:square_root_results)
 end
 
 get("/") do
